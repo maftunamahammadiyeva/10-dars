79284c6e139ca4e7a4956d95eb41b2b5b24a370b
maftunamahammadiyeva <mahammadiyevamaftuna01@gmail.com>
2022-06-05 22:42:00 +0500

    Initial

---
+++ js/index.js
@@ -0,0 +1,25 @@
+const elForm = document.querySelector (".form");
+const elInput = document.querySelector (".input");
+const elFormInput = document.querySelector (".form__input");
+const elContainer = document.querySelector (".container");
+const todos =[]
+ elForm.addEventListener ("submit", function (evt){
+   evt.preventDefault()
+   const todoValue = elInput.value;
+   const nameValue = elFormInput.value;
+
+   const list ={
+    name:nameValue,
+    todo:todoValue
+   }
+   todos.push(list);
+  //  todoValue=null;
+  //  nameValue=null;
+   for (let tod of todos){
+     let nameP=document.createElement("p");
+
+     elContainer.appendChild(nameP)
+     nameP.textContent=`${tod.name} (${tod.todo})`
+   }
+
+ })
